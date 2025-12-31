3db1d7f4be3973143beb431d56982b7ef5abdb2d
Bernardo <bernardoguimaraesnp@gmail.com>
2025-12-31 11:46:27 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,6 +11,9 @@ import java.util.List;
 // Entity transforma uma classe me entidade do banco de dados
 @Entity
 @Table(name = "tb_cadastro") // Definindo o nome da tabela, por padrao as tabelas não terão nome
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -26,36 +32,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreign Key (chave estrangeira)
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
