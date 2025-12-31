3d8b5573403f7475f4dac0bb243d93d06ff0643f
Bernardo <bernardoguimaraesnp@gmail.com>
2025-12-31 13:43:36 -0300

    feat: Cria os arquivos Service e Repository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
